4b9a4dd2a342d7d92c349eb82eb807c5adacda2d
Page Telegram <43476661+pagetelegram@users.noreply.github.com>
2020-08-20 14:03:29 -0500

    Update install_fbc_pi.sh

--- install_fbc_pi.sh
+++ install_fbc_pi.sh
@@ -2,11 +2,11 @@
 
 #For RPi 1 replace the first line by:
 #wget http://users.freebasic-portal.de/stw/builds/linux-armv6-rpi/freebasic.deb
-
+sudo apt install gcc make lib{ncurses5,gpm,x11,xext,xpm,xrandr,xrender,gl1-mesa,ffi}-dev
 wget http://users.freebasic-portal.de/stw/builds/linux-armv7a-hf-debian/freebasic.deb
 wget http://users.freebasic-portal.de/stw/builds/freebasic-headers.deb
 sudo dpkg -i freebasic.deb
 sudo dpkg -i freebasic-headers.deb
-apt-get update
-apt-get install -f
+sudo apt-get update
+sudo apt-get install -f
 
